e39ed60508e9c6243e5372bafb111379dba68887
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-21 03:47:18 +0800

    apollo: Drop selinux context from batterysecret init unit
    We do that from sepolicy label

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,9 @@ EOF
         vendor/etc/init/init.mi_thermald.rc)
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
+        vendor/etc/init/init.batterysecret.rc)
+            sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
+            ;;
     esac
 }
 
