9a820f0b3881dfe6fbc92b7e7f61a0f3fff404ed
Ahmed Tohamy <1ahmed.tohamy@gmail.com>
2024-09-26 18:38:10 +0300

    apollo: Adapt for nexus kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CONFIG += vendor/xiaomi/apollo.config
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_CLANG_VERSION := r487747c
+TARGET_KERNEL_CONFIG := \
+    vendor/kona-perf_defconfig \
+    vendor/debugfs.config \
+    vendor/xiaomi/sm8250-common.config
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
