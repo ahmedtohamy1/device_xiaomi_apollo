28af73b948b49856d518cd596540e2e20c9da2f8
Tim Zimmermann <tim@linux4.de>
2024-09-30 08:07:13 +0000

    apollo: Switch to AIDL Fingerprint HAL - As of Android 15, AIDL Fingerprint HAL is required to set fingerprint icon indicator pos. - Drop all the HIDL fingerprint overlays - Satisfy FCM

--- device.mk
+++ device.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/fingerprint/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
